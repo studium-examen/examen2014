ffd609df2741d14ee6310db96214363cc7afd7eb
Steve Massicotte <steve.massicotte@umontreal.ca>
2014-08-29 14:49:10 -0400

    Mise en place de la structure.

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <title>Application ABC</title>
+
+  <link rel="stylesheet" href="css/site.css">
+
+</head>
+
+<body>
+    <?php echo "Version 1"; ?>
+</body>
+</html>
